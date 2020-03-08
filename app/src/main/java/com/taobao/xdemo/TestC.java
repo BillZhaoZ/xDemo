737b9b85d5284487a7a7a4bf89e035905b4eaf9b
露明 <zhaozhuang.zz@alibaba-inc.com>
2020-03-08 11:48:19 +0800

    提交

--- app/src/main/java/com/taobao/xdemo/TestC.java
+++ app/src/main/java/com/taobao/xdemo/TestC.java
@@ -6,11 +6,11 @@ public class TestC {
 
     public static void main(String[] args) {
 
-        hahahMater();
+        hahahMater1111();
 
     }
 
-    private static void hahahMater() {
+    private static void hahahMater1111() {
 
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
 
