d089e7fc8bd5f1c68340f67a061af843901f0527
露明 <zhaozhuang.zz@alibaba-inc.com>
2020-03-08 11:56:12 +0800

    ceshi

--- app/src/main/java/com/taobao/xdemo/TestC.java
+++ app/src/main/java/com/taobao/xdemo/TestC.java
@@ -5,21 +5,10 @@ import java.util.HashMap;
 public class TestC {
 
     public static void main(String[] args) {
-
-<<<<<<< HEAD
         hahahMater2();
-
     }
 
     private static void hahahMater2() {
-=======
-        hahahMater1();
-
-    }
-
-    private static void hahahMater1() {
->>>>>>> bugfix
-
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
 
         objectObjectHashMap.put("cc", "cccc");
